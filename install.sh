9af20f826c162a97c9dd0343e309e597523393c7
medvedicos <41338299+medvedicos@users.noreply.github.com>
2025-09-09 22:30:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+/etc/init.d/zapret stop
+
 #Скачивание конфига
 curl -f https://raw.githubusercontent.com/medvedicos/getconfig/refs/heads/main/getconfig --output /etc/init.d/getconfig
 chmod +x /etc/init.d/getconfig
 /etc/init.d/getconfig enable
+/etc/init.d/getconfig start
 
 curl -f https://raw.githubusercontent.com/medvedicos/getconfig/refs/heads/main/getexclude --output /etc/init.d/getexclude
 chmod +x /etc/init.d/getexclude
 /etc/init.d/getexclude enable
+/etc/init.d/getexclude start
 
 echo "4 0 * * 1  /etc/init.d/getconfig" >> /etc/crontabs/root
 echo "4 0 * * 1  /etc/init.d/getexclude" >> /etc/crontabs/root
