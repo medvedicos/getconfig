a0fcbde540f4f7492a19410d6a26ad6efb220a0f
medvedicos <41338299+medvedicos@users.noreply.github.com>
2025-09-17 00:39:26 +0300

    Update install.sh

--- kaliningrad/tis-dialog/install.sh
+++ kaliningrad/tis-dialog/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #Скачивание конфига
-curl -f https://raw.githubusercontent.com/medvedicos/getconfig/refs/heads/main/getconfig --output /etc/init.d/getconfig
+curl -f https://raw.githubusercontent.com/medvedicos/getconfig/refs/heads/main/kaliningrad/tis-dialog/getconfig --output /etc/init.d/getconfig
 chmod +x /etc/init.d/getconfig
 /etc/init.d/getconfig enable
 /etc/init.d/getconfig start
 
+#Скачивание исключений
 curl -f https://raw.githubusercontent.com/medvedicos/getconfig/refs/heads/main/getexclude --output /etc/init.d/getexclude
 chmod +x /etc/init.d/getexclude
 /etc/init.d/getexclude enable
