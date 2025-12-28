c0446bd6ae0e4d244d133d619c6d4811e4e4b0f8
medvedicos <41338299+medvedicos@users.noreply.github.com>
2025-12-28 17:08:31 +0200

    Add files via upload

---
+++ Beeline/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Скачивание конфига
+curl -f https://raw.githubusercontent.com/medvedicos/getconfig/refs/heads/main/getconfig --output /etc/init.d/getconfig
+chmod +x /etc/init.d/getconfig
+/etc/init.d/getconfig enable
+/etc/init.d/getconfig start
+
+curl -f https://raw.githubusercontent.com/medvedicos/getconfig/refs/heads/main/getexclude --output /etc/init.d/getexclude
+chmod +x /etc/init.d/getexclude
+/etc/init.d/getexclude enable
+/etc/init.d/getexclude start
+
+echo "4 0 * * 1  /etc/init.d/getconfig" >> /etc/crontabs/root
+echo "4 0 * * 1  /etc/init.d/getexclude" >> /etc/crontabs/root
